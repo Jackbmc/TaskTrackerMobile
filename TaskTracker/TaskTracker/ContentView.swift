05d527b0ffbeca376960041ff859f39fde58d758
Jax2208 <JackMc2208@gmail.com>
2023-03-21 19:50:01 +1100

    Moved checkboxes, add clear all, implemented editing

--- TaskTracker/TaskTracker/ContentView.swift
+++ TaskTracker/TaskTracker/ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var tasks = [Task]()
     @State private var newTaskTitle = ""
+    @State private var isEditing = false
+    @State private var editingTask: Task?
     
     private func loadTasks() {
         if let data = UserDefaults.standard.data(forKey: "tasks"),
@@ -36,15 +38,43 @@ struct ContentView: View {
         saveTasks()
     }
     
+    private func clearAllTasks() {
+        tasks.removeAll()
+        saveTasks()
+    }
+    
+    private func startEditing(task: Task) {
+        editingTask = task
+        newTaskTitle = task.title
+        isEditing = true
+    }
+    
+    private func updateTask() {
+        if let task = editingTask,
+           let index = tasks.firstIndex(where: { $0.id == task.id }) {
+            tasks[index].title = newTaskTitle
+            saveTasks()
+            isEditing = false
+            newTaskTitle = ""
+            editingTask = nil
+        }
+    }
+    
+    private func cancelEditing() {
+        isEditing = false
+        newTaskTitle = ""
+        editingTask = nil
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
                 HStack {
-                    TextField("Enter a new task...", text: $newTaskTitle)
+                    TextField(isEditing ? "Edit task..." : "Enter a new task...", text: $newTaskTitle)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     
-                    Button(action: addTask) {
-                        Image(systemName: "arrow.up.circle.fill")
+                    Button(action: isEditing ? updateTask : addTask) {
+                        Image(systemName: isEditing ? "pencil.circle.fill" : "arrow.up.circle.fill")
                             .resizable()
                             .frame(width: 24, height: 24)
                             .foregroundColor(.blue)
@@ -59,11 +89,16 @@ struct ContentView: View {
                             Button(action: {
                                 toggleTaskCompletion(task: task)
                             }) {
-                                Text(task.title)
-                                    .strikethrough(task.isCompleted, color: .red)
-                                Spacer()
                                 Image(systemName: task.isCompleted ? "checkmark.square" : "square")
                             }
+                            Text(task.title)
+                                .strikethrough(task.isCompleted, color: .red)
+                                .onTapGesture {
+                                    toggleTaskCompletion(task: task)
+                                }
+                                .onLongPressGesture {
+                                    startEditing(task: task)
+                                }
                         }
                         .padding()
                     }
@@ -72,6 +107,11 @@ struct ContentView: View {
             }
             .onAppear(perform: loadTasks)
             .navigationTitle("TaskTracker")
+            .navigationBarItems(leading: isEditing ? Button(action: cancelEditing) {
+                Text("Cancel")
+            } : nil, trailing: Button(action: clearAllTasks) {
+                Text("Clear All")
+            })
         }
     }
 }
