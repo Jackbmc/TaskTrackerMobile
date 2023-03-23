e9270e09b0cbd0eaca9544aa5ada3ccb8943c18b
Callum Bennett <66990734+CyberTapir@users.noreply.github.com>
2023-03-23 21:00:09 +1100

    Removed trailing whitespace

--- TaskTracker/TaskTracker/ContentView.swift
+++ TaskTracker/TaskTracker/ContentView.swift
@@ -1,26 +1,22 @@
 import SwiftUI
 import UserNotifications
-
 struct ContentView: View {
     @State private var tasks = [Task]()
     @State private var newTaskTitle = ""
     @State private var newTaskDueDate = Date()
     @State private var isEditing = false
     @State private var editingTask: Task?
-    
     private func loadTasks() {
         if let data = UserDefaults.standard.data(forKey: "tasks"),
            let storedTasks = try? JSONDecoder().decode([Task].self, from: data) {
             tasks = storedTasks
         }
     }
-    
     private func saveTasks() {
         if let data = try? JSONEncoder().encode(tasks) {
             UserDefaults.standard.set(data, forKey: "tasks")
         }
     }
-    
     private func addTask() {
         let newTask = Task(title: newTaskTitle, isCompleted: false, dueDate: newTaskDueDate)
         tasks.append(newTask)
@@ -28,14 +24,12 @@ struct ContentView: View {
         scheduleNotification(for: newTask) // Schedule a notification for the new task
         newTaskTitle = ""
     }
-    
     private func toggleTaskCompletion(task: Task) {
         if let index = tasks.firstIndex(where: { $0.id == task.id }) {
             tasks[index].isCompleted.toggle()
             saveTasks()
         }
     }
-    
     private func deleteTask(at offsets: IndexSet) {
         offsets.forEach { index in
             removeNotification(for: tasks[index])
@@ -46,19 +40,16 @@ struct ContentView: View {
     private func removeNotification(for task: Task) {
         UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [task.id.uuidString])
     }
-    
     private func clearAllTasks() {
         tasks.removeAll()
         saveTasks()
     }
-    
     private func startEditing(task: Task) {
         editingTask = task
         newTaskTitle = task.title
         newTaskDueDate = task.dueDate ?? Date() // Set the due date picker to the task's due date
         isEditing = true
     }
-    
     private func updateTask() {
         if let task = editingTask,
            let index = tasks.firstIndex(where: { $0.id == task.id }) {
@@ -71,18 +62,15 @@ struct ContentView: View {
             scheduleNotification(for: tasks[index]) // Schedule a notification for the updated task
         }
     }
-    
     private func cancelEditing() {
         isEditing = false
         newTaskTitle = ""
         editingTask = nil
     }
-    
     private func isTaskOverdue(task: Task) -> Bool {
         guard let dueDate = task.dueDate else { return false }
         return Date() > dueDate
     }
-    
     private func sortedTasks() -> [Task] {
         return tasks.sorted(by: { (task1, task2) -> Bool in
             if let dueDate1 = task1.dueDate, let dueDate2 = task2.dueDate {
@@ -94,7 +82,6 @@ struct ContentView: View {
             }
         })
     }
-    
     private func requestNotificationPermissions() {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, _ in
             if granted {
@@ -104,7 +91,6 @@ struct ContentView: View {
             }
         }
     }
-    
     private func scheduleNotification(for task: Task) {
         guard let dueDate = task.dueDate else { return }
         
@@ -123,14 +109,12 @@ struct ContentView: View {
             }
         }
     }
-    
     var body: some View {
         NavigationView {
             VStack {
                 HStack {
                     TextField(isEditing ? "Edit task..." : "Enter a new task...", text: $newTaskTitle)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
-                    
                     Button(action: isEditing ? updateTask : addTask) {
                         Image(systemName: isEditing ? "pencil.circle.fill" : "arrow.up.circle.fill")
                             .resizable()
@@ -140,7 +124,6 @@ struct ContentView: View {
                     .disabled(newTaskTitle.isEmpty)
                 }
                 .padding()
-                
                 HStack {
                     Text("Due date/time:")
                     DatePicker("", selection: $newTaskDueDate, displayedComponents: [.date, .hourAndMinute])
@@ -148,7 +131,6 @@ struct ContentView: View {
                         .labelsHidden()
                 }
                 .padding()
-                
                 List {
                     ForEach(sortedTasks()) { task in
                         HStack {
@@ -190,13 +172,11 @@ struct ContentView: View {
             })
         }
     }
-    
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
         }
     }
-    
     private let formatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .short
